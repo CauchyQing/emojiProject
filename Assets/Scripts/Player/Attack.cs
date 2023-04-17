09174675502950fe2c9283005a7c4744a1494f17
lihongwwang <106501454+lihongwwang@users.noreply.github.com>
2023-04-17 23:02:08 +0800

    wlh

---
+++ Assets/Scripts/Player/Attack.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attack : MonoBehaviour
+{
+    public int damage;  //增加击飞概率
+    public float attackRange;
+    public float attackRate; //攻击频率
+
+
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        other.GetComponent<playerAttribution>().TakeDamage(this);
+    }
+
+
+
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/Player/playerAttribution.cs
+++ Assets/Scripts/Player/playerAttribution.cs
@@ -1,14 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class playerAttribution : MonoBehaviour
 {
+    [Header("基本属性")]
+    public float strikeOdds;    //初始击飞概率
+    public float currentOdds;   //当前击飞概率
+
+
+
+
+    public UnityEvent<playerAttribution> OnOddsChange;  //受伤改变击飞概率
+    public UnityEvent<Transform> OnTakeDamage;  //受伤动画
+    public UnityEvent Ondie;
+
+
     [Header("检测参数")]
     public float checkRaduis;
     public Vector2 bottomOffset;
     public LayerMask groundLayer;
     public bool isGround;
+
 
     private void Update()
     {
@@ -24,5 +38,19 @@ public class playerAttribution : MonoBehaviour
     {
         Gizmos.DrawWireSphere((Vector2)transform.position + bottomOffset, checkRaduis);
     }
-   
+
+    private void Start()
+    {
+        currentOdds = strikeOdds;
+    }
+
+    public void TakeDamage(Attack attacker)
+    {
+        currentOdds += attacker.damage;
+        OnOddsChange?.Invoke(this);
+        OnTakeDamage?.Invoke(attacker.transform);
+    }
+
+
+
 }
