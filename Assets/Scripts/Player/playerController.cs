2bfa9a43fc54904ff236ca70664b84c053feee66
卿柯序 <1415708632@qq.com>
2023-04-18 20:55:49 +0800

    qkx

--- Assets/Scripts/Player/playerController.cs
+++ Assets/Scripts/Player/playerController.cs
@@ -11,7 +11,7 @@ public class playerController : MonoBehaviour
     public PlayerInputSystem inputControl;
     public playerAnimation playerAnimation;
     public Vector2 inputDirection;
-    
+
     [Header("基本参数")]
     public float speed;
     public float jumpForce;
@@ -19,16 +19,16 @@ public class playerController : MonoBehaviour
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
-        playerAttribution = GetComponent<playerAttribution>();  
-        inputControl=new PlayerInputSystem();
-        playerAnimation=GetComponent<playerAnimation>();
+        playerAttribution = GetComponent<playerAttribution>();
+        inputControl = new PlayerInputSystem();
+        playerAnimation = GetComponent<playerAnimation>();
         inputControl.GamePlay.Jump.started += Jump;
 
         inputControl.GamePlay.Fire.started += NormalAttack;
 
     }
 
-   
+
 
     private void OnEnable()
     {
@@ -40,7 +40,7 @@ public class playerController : MonoBehaviour
     }
     private void Update()
     {
-        inputDirection=inputControl.GamePlay.Move.ReadValue<Vector2>(); 
+        inputDirection = inputControl.GamePlay.Move.ReadValue<Vector2>();
     }
 
     private void FixedUpdate()
@@ -50,28 +50,28 @@ public class playerController : MonoBehaviour
 
     public void Move()
     {
-        rb.velocity = new Vector2(inputDirection.x * speed * Time.deltaTime, rb.velocity.y); 
-        
+        rb.velocity = new Vector2(inputDirection.x * speed * Time.deltaTime, rb.velocity.y);
+
 
         //人物翻转
-        int faceDirection =(int)transform.localScale.x;
-        if(inputDirection.x < 0)
+        int faceDirection = (int)transform.localScale.x;
+        if (inputDirection.x < 0)
             faceDirection = -1;
-        else if(inputDirection.x > 0)
+        else if (inputDirection.x > 0)
             faceDirection = 1;
         transform.localScale = new Vector3(faceDirection, 1, 1);
     }
 
     private void Jump(InputAction.CallbackContext obj)
     {
-        if(playerAttribution.isGround)
-            rb.AddForce(transform.up * jumpForce, ForceMode2D.Impulse); 
+        if (playerAttribution.isGround)
+            rb.AddForce(transform.up * jumpForce, ForceMode2D.Impulse);
     }
 
 
     private void NormalAttack(InputAction.CallbackContext obj)
     {
-        isNormalAttack= true;
+        isNormalAttack = true;
         playerAnimation.PlayerNormalAttack();
         playerAttribution.OnAttack?.Invoke(playerAttribution);
     }
@@ -80,9 +80,9 @@ public class playerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
-   
+
 }
