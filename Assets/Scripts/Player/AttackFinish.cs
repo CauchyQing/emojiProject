f8c60ccbaad9bf5c02bf02bd9eec3c13d4ea146e
lihongwwang <106501454+lihongwwang@users.noreply.github.com>
2023-04-23 21:48:43 +0800

    JiaPei

--- Assets/Scripts/Player/AttackFinish.cs
+++ Assets/Scripts/Player/AttackFinish.cs
@@ -2,13 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+
 public class AttackFinish : StateMachineBehaviour
 {
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
-    //override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    //{
-    //    
-    //}
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        animator.GetComponent<playerController>().isNormalAttack = true;
+    }
 
     // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
     //override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
@@ -19,7 +21,7 @@ public class AttackFinish : StateMachineBehaviour
     // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
     override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        animator.SetBool("isNormalAttack",false);
+        animator.GetComponent<playerController>().isNormalAttack=false;
     }
 
     // OnStateMove is called right after Animator.OnAnimatorMove()
