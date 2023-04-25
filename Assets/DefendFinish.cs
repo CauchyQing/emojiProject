9de271bb792d0f89d5ed85d546685c82a7147fd5
lihongwwang <106501454+lihongwwang@users.noreply.github.com>
2023-04-25 22:20:19 +0800

    wlh

--- Assets/DefendFinish.cs
+++ Assets/DefendFinish.cs
@@ -16,11 +16,11 @@ public class DefendFinish : StateMachineBehaviour
     //    
     //}
 
-    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
-    //override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    //{
-    //    
-    //}
+   // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        animator.GetComponent<playerController>().isDefend = false;
+    }
 
     // OnStateMove is called right after Animator.OnAnimatorMove()
     //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
