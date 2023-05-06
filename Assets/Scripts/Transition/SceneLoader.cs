b5914b28946c3eb8e2ad5c0ea72db8a7d1f6c3b0
卿柯序 <1415708632@qq.com>
2023-05-06 22:47:20 +0800

    Qing Kexu——场景加载

--- Assets/Scripts/Transition/SceneLoader.cs
+++ Assets/Scripts/Transition/SceneLoader.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,10 +7,57 @@ using UnityEngine.SceneManagement;
 
 public class SceneLoader : MonoBehaviour
 {
+    [Header("事件监听")]
+    public SceneLoadEventSO loadEventSO;
+
     public GameSceneSO firstLoadScene;
+
+    private GameSceneSO currentLoadScene;
+    private GameSceneSO sceneToLoad;
+    private Vector3 positionToGo;
+    private bool fadeScreen;
+
+    public float fadeDuration;
 
     private void Awake()
     {
         Addressables.LoadSceneAsync(firstLoadScene.sceneReference, LoadSceneMode.Additive);
+        currentLoadScene = firstLoadScene;
     }
+
+    private void OnEnable()
+    {
+        loadEventSO.loadRequestEvent += OnLoadRequestEvent;
+    }
+
+    private void OnDisable()
+    {
+        loadEventSO.loadRequestEvent -= OnLoadRequestEvent;
+
+    }
+
+    private void OnLoadRequestEvent(GameSceneSO locationToLoad, Vector3 posToGo, bool fadeScreen)
+    {
+        sceneToLoad = locationToLoad;
+        positionToGo = posToGo;
+        this.fadeScreen = fadeScreen;
+
+
+    }
+
+    private IEnumerator UnLoadPreviousScene()
+    {
+        if (fadeScreen)
+        {
+            //TODO:实现渐入渐出
+        }
+
+        //等待一定秒数
+        yield return new WaitForSeconds(fadeDuration);
+
+        if (currentLoadScene != null)
+            currentLoadScene.sceneReference.UnLoadScene();
+    }
+
+    //private void Load
 }
