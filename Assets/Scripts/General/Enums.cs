ae4eddb89e8dfde6df89aaadc5fefc09b0c5e09d
卿柯序 <1415708632@qq.com>
2023-05-06 22:13:19 +0800

    Qing Kexu——场景加载

---
+++ Assets/Scripts/General/Enums.cs
@@ -0,0 +1,4 @@
+public enum SceneType
+{
+    Menu, ChooseCharacter, Map
+}
--- Assets/Scripts/Transition/SceneLoader.cs
+++ Assets/Scripts/Transition/SceneLoader.cs
@@ -1,18 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.SceneManagement;
 
 public class SceneLoader : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public GameSceneSO firstLoadScene;
 
-    // Update is called once per frame
-    void Update()
+    private void Awake()
     {
-        
+        Addressables.LoadSceneAsync(firstLoadScene.sceneReference, LoadSceneMode.Additive);
     }
 }
