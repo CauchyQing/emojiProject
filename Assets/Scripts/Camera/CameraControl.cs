875f6fe429244a586f84ca974fc73dda71e42ee0
卿柯序 <1415708632@qq.com>
2023-05-07 20:30:24 +0800

    Qing Kexu——场景加载

--- Assets/Scripts/Camera/CameraControl.cs
+++ Assets/Scripts/Camera/CameraControl.cs
@@ -6,6 +6,9 @@ using System;
 
 public class CameraControl : MonoBehaviour
 {
+    [Header("ÊÂ¼þ¼àÌý")]
+    public VoidEventSO afterSceneLoadedEvent;
+
     private CinemachineConfiner2D confiner2D;
     public CinemachineImpulseSource impulseSource;
     public VoidEventSO cameraShakeEvent;
@@ -18,11 +21,18 @@ public class CameraControl : MonoBehaviour
     private void OnEnable()
     {
         cameraShakeEvent.OnEventRaised += OnCameraShakeEvent;
+        afterSceneLoadedEvent.OnEventRaised += OnAfterSceneLoadedEvent;
     }
 
     private void OnDisable()
     {
         cameraShakeEvent.OnEventRaised -= OnCameraShakeEvent;
+        afterSceneLoadedEvent.OnEventRaised -= OnAfterSceneLoadedEvent;
+    }
+
+    private void OnAfterSceneLoadedEvent()
+    {
+        GetNewCameraBounds();
     }
 
     private void OnCameraShakeEvent()
@@ -30,20 +40,14 @@ public class CameraControl : MonoBehaviour
         impulseSource.GenerateImpulse();
     }
 
-    //
-    /*    private void Start()
-        {
-            GetNewCameraBounds();
-        }
+    private void GetNewCameraBounds()
+    {
+        var obj = GameObject.FindGameObjectWithTag("Bounds");
+        if (obj == null)
+            return;
 
-        private void GetNewCameraBounds()
-        {
-            var obj = GameObject.FindGameObjectWithTag("Bounds");
-            if (obj == null)
-                return;
+        confiner2D.m_BoundingShape2D = obj.GetComponent<Collider2D>();
 
-            confiner2D.m_BoundingShape2D = obj.GetComponent<Collider2D>();
-
-            confiner2D.InvalidateCache();
-        }*/
+        confiner2D.InvalidateCache();
+    }
 }
