666e457a30978a2ed56724e7d61eaaa779626fd0
卿柯序 <1415708632@qq.com>
2023-05-07 23:10:58 +0800

    ygw

--- Assets/Scripts/Map/Spike.cs
+++ Assets/Scripts/Map/Spike.cs
@@ -24,12 +24,12 @@ public class Spike : MonoBehaviour
         if (collision.gameObject.layer == LayerMask.NameToLayer("Player") && collision.gameObject.GetComponent<Rigidbody2D>().velocity.y <= 0)
         {
 
-            collision.gameObject.gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(0, 0);
+            //collision.gameObject.gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(0, 0);
 
 
 
             //collision.gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0, 1) * force, ForceMode2D.Impulse);
-            collision.gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(gameObject.GetComponent<Transform>().localScale.x * (-1), 1) * force, ForceMode2D.Impulse);
+            collision.gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(gameObject.GetComponent<Transform>().localScale.x, -1) * force * (-1), ForceMode2D.Impulse);
 
         }
     }
