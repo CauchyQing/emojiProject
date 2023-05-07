49ec9a09913316662fb6a8531c7ebdaa9d98f383
卿柯序 <1415708632@qq.com>
2023-05-07 22:59:43 +0800

    Qing Kexu

--- Assets/Scripts/UI/FadeCanvas.cs
+++ Assets/Scripts/UI/FadeCanvas.cs
@@ -2,8 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using DG.Tweening;
 
 public class FadeCanvas : MonoBehaviour
 {
     public Image fadeImage;
+
+    private void OnFadeEvent(Color target, float duration)
+    {
+        fadeImage.DOBlendableColor(target, duration);
+    }
 }
