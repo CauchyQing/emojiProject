8519e12ebeed2d9d648b9fdf16ee86d9adc3a3c6
卿柯序 <1415708632@qq.com>
2023-05-08 20:05:15 +0800

    Qing Kexu

--- Assets/Scripts/Player/playerAttribution.cs
+++ Assets/Scripts/Player/playerAttribution.cs
@@ -11,15 +11,15 @@ public class playerAttribution : MonoBehaviour
     public playerAnimation anim;
     public Rigidbody2D rb;
     public playerController pc;
-    
+
     [Header("基本属性")]
-    public float health=100;    //初始血量
+    public float health = 100;    //初始血量
     public float currentHealth;   //当前血量
 
     public UnityEvent<playerAttribution> OnAttack;  //攻击事件
     public UnityEvent<Transform> OnTakeDamage;  //受伤事件
     public UnityEvent Ondie;        //死亡事件
-
+    public UnityEvent SuccessDefend;        //振刀成功
 
     [Header("检测参数")]
     public float checkRaduis;
@@ -28,24 +28,25 @@ public class playerAttribution : MonoBehaviour
     public LayerMask platformLayer;//平台检测层
     public bool isGround;
     public bool isPlatform;//判断是否在平台
-    
-    public enum STATE { 
+
+    public enum STATE
+    {
         NOTHING,
         DEFEND,
         NORMALATTACK,
         ACCUMULATEATTACK
     }
-    public STATE state=STATE.NOTHING;
+    public STATE state = STATE.NOTHING;
 
     private void Update()
     {
         Check();
         updateSTATE();
-       
+
     }
     public void updateSTATE()
     {
-        if (pc.isDefend) 
+        if (pc.isDefend)
             state = STATE.DEFEND;
         else if (pc.isAccumulate)
             state = STATE.ACCUMULATEATTACK;
@@ -54,7 +55,7 @@ public class playerAttribution : MonoBehaviour
         else
             state = STATE.NOTHING;
 
-        
+
     }
 
     public void Check()
@@ -72,9 +73,9 @@ public class playerAttribution : MonoBehaviour
     private void Start()
     {
         currentHealth = health;
-        anim= GetComponent<playerAnimation>();
+        anim = GetComponent<playerAnimation>();
         rb = GetComponent<Rigidbody2D>();
-        pc=GetComponent<playerController>();
+        pc = GetComponent<playerController>();
     }
 
     public void TakeDamage(Attack attacker, STATE s)
@@ -90,52 +91,52 @@ public class playerAttribution : MonoBehaviour
             TakeDamageHelp(attacker, (float)1.5);
         }
 
-         else if(s==STATE.ACCUMULATEATTACK && state == STATE.DEFEND)
-         {
-             attacker.pa.currentHealth -= attacker.damage*2;
-             OnTakeDamage?.Invoke(this.transform);
-             OnTakeDamage?.Invoke(this.transform);
+        else if (s == STATE.ACCUMULATEATTACK && state == STATE.DEFEND)
+        {
+            attacker.pa.currentHealth -= attacker.damage * 2;
+            OnTakeDamage?.Invoke(this.transform);
+            SuccessDefend?.Invoke();
 
-             if (attacker.pa.currentHealth <= 0)
-             {
-                 attacker.pa.currentHealth = 0;
-                 attacker.pa.Ondie?.Invoke();
-                 attacker.pa.rb.AddForce(2 * attacker.pa.transform.up * attacker.attackForce , ForceMode2D.Impulse);
-                 if (attacker.tf.position.x < transform.position.x)
-                     attacker.pa.rb.AddForce(transform.right * attacker.attackForce * 5 , ForceMode2D.Impulse);
-                 else
-                     attacker.pa.rb.AddForce(-1 * transform.right * attacker.attackForce * 5 , ForceMode2D.Impulse);
-                 attacker.pa.anim.PlayerHurt();
-             }
-             else
-             {
-                 attacker.pa.rb.AddForce(2 * attacker.pa.transform.up * attacker.attackForce , ForceMode2D.Impulse);
-                 if (attacker.tf.position.x < transform.position.x)
-                     attacker.pa.rb.AddForce(attacker.pa.transform.right * attacker.attackForce , ForceMode2D.Impulse);
-                 else
-                     attacker.pa.rb.AddForce(-1 * attacker.pa.transform.right * attacker.attackForce , ForceMode2D.Impulse);
-                 attacker.pa.anim.PlayerHurt();
-             }
-         }
+            if (attacker.pa.currentHealth <= 0)
+            {
+                attacker.pa.currentHealth = 0;
+                attacker.pa.Ondie?.Invoke();
+                attacker.pa.rb.AddForce(2 * attacker.pa.transform.up * attacker.attackForce, ForceMode2D.Impulse);
+                if (attacker.tf.position.x < transform.position.x)
+                    attacker.pa.rb.AddForce(transform.right * attacker.attackForce * 5, ForceMode2D.Impulse);
+                else
+                    attacker.pa.rb.AddForce(-1 * transform.right * attacker.attackForce * 5, ForceMode2D.Impulse);
+                attacker.pa.anim.PlayerHurt();
+            }
+            else
+            {
+                attacker.pa.rb.AddForce(2 * attacker.pa.transform.up * attacker.attackForce, ForceMode2D.Impulse);
+                if (attacker.tf.position.x < transform.position.x)
+                    attacker.pa.rb.AddForce(attacker.pa.transform.right * attacker.attackForce, ForceMode2D.Impulse);
+                else
+                    attacker.pa.rb.AddForce(-1 * attacker.pa.transform.right * attacker.attackForce, ForceMode2D.Impulse);
+                attacker.pa.anim.PlayerHurt();
+            }
+        }
 
     }
 
-    public void TakeDamageHelp(Attack attacker,float x)
+    public void TakeDamageHelp(Attack attacker, float x)
     {
-        currentHealth -= attacker.damage*x;
+        currentHealth -= attacker.damage * x;
         OnTakeDamage?.Invoke(attacker.transform);
 
         if (currentHealth <= 0)
         {
             currentHealth = 0;
             Ondie?.Invoke();
-            rb.AddForce(2*transform.up * attacker.attackForce*x, ForceMode2D.Impulse);
+            rb.AddForce(2 * transform.up * attacker.attackForce * x, ForceMode2D.Impulse);
             anim.PlayerHurt();
             if (attacker.tf.position.x < transform.position.x)
-                rb.AddForce(transform.right * attacker.attackForce * 5*x, ForceMode2D.Impulse);
+                rb.AddForce(transform.right * attacker.attackForce * 5 * x, ForceMode2D.Impulse);
             else
-                rb.AddForce(-1 * transform.right * attacker.attackForce * 5*x, ForceMode2D.Impulse);
-     
+                rb.AddForce(-1 * transform.right * attacker.attackForce * 5 * x, ForceMode2D.Impulse);
+
         }
         else
         {
@@ -144,9 +145,9 @@ public class playerAttribution : MonoBehaviour
                 rb.AddForce(transform.right * attacker.attackForce * x, ForceMode2D.Impulse);
             else
                 rb.AddForce(-1 * transform.right * attacker.attackForce * x, ForceMode2D.Impulse);
-           
+
         }
     }
-    
+
 
 }
