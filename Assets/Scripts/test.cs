10c1e2b7f6ae5a1c072ff7b43f4371bb3395627a
卿柯序 <1415708632@qq.com>
2023-05-09 21:13:59 +0800

    re

--- Assets/Scripts/test.cs
+++ Assets/Scripts/test.cs
@@ -9,7 +9,7 @@ public class test : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("test");//564441
+        Debug.Log("test");//5644411
     }
 
     // Update is called once per frame
