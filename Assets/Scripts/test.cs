b0c14296b308d8d768fb65dc52d42b98cf42e111
2230088380@qq.com <2230088380@qq.com>
2023-05-09 20:59:20 +0800

    0

--- Assets/Scripts/test.cs
+++ Assets/Scripts/test.cs
@@ -9,7 +9,8 @@ public class test : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("test");//564441
+        Debug.Log("test123");//564441
+        
     }
 
     // Update is called once per frame
