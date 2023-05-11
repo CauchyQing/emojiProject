e0cc34fa88c1de2246be2b338849a305d2096c07
卿柯序 <1415708632@qq.com>
2023-05-11 23:07:50 +0800

    Qing Kexu——场景转换

--- Assets/Scripts/ScriptableObject/SceneLoadEventSO.cs
+++ Assets/Scripts/ScriptableObject/SceneLoadEventSO.cs
@@ -4,16 +4,14 @@ using UnityEngine.Events;
 [CreateAssetMenu(menuName = "Event/SceneLoadEventSO")]
 public class SceneLoadEventSO : ScriptableObject
 {
-    public UnityAction<GameSceneSO, Vector3, bool> loadRequestEvent;
+    public UnityAction<GameSceneSO> loadRequestEvent;
 
     /// <summary>
     /// 场景加载请求
     /// </summary>
     /// <param name="locationToLoad">要加载的场景</param>
-    /// <param name="posToGo">Player的目的坐标</param>
-    /// <param name="fadeScreen">是否渐入渐出</param>
-    public void RaiseLoadRequestEvent(GameSceneSO locationToLoad, Vector3 posToGo, bool fadeScreen)
+    public void RaiseLoadRequestEvent(GameSceneSO locationToLoad)
     {
-        loadRequestEvent?.Invoke(locationToLoad, posToGo, fadeScreen);
+        loadRequestEvent?.Invoke(locationToLoad);
     }
 }
--- Assets/Scripts/UI/CharacterCreation.cs
+++ Assets/Scripts/UI/CharacterCreation.cs
@@ -4,57 +4,56 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class CharacterCreation : MonoBehaviour
+{
+    public GameObject[] characterPrefabs;
+    private GameObject[] characterGameObjects;
+    private int selectedIndex = 0;
+    private int length;//所有可供选择的角色个数 
+    void Start()
     {
-        public GameObject[] characterPrefabs;
-        private GameObject[] characterGameObjects;
-        private int selectedIndex = 0;
-        private int length;//所有可供选择的角色个数 
-        void Start()
+        length = characterPrefabs.Length;
+        characterGameObjects = new GameObject[length];
+        for (int i = 0; i < length; i++)
         {
-            length = characterPrefabs.Length;
-            characterGameObjects = new GameObject[length];
-            for (int i = 0; i < length; i++)
-            {
-                characterGameObjects[i] = GameObject.Instantiate(characterPrefabs[i], transform.position, transform.rotation) as GameObject;
-            }
-            UpdateCharacterShow();
+            characterGameObjects[i] = GameObject.Instantiate(characterPrefabs[i], transform.position, transform.rotation) as GameObject;
         }
-
-        void UpdateCharacterShow()//更新所有角色的显示
-        {
-            characterGameObjects[selectedIndex].SetActive(true);
-            for (int i = 0; i < length; i++)
-            {
-                if (i != selectedIndex)
-                {
-                    characterGameObjects[i].SetActive(false);//把未选择的角色隐藏起来
-                }
-            }
-        }
-        public void OnNextButtonClick()//当我们点击了下一个按钮
-        {
-            selectedIndex++;
-            selectedIndex %= length;
-            UpdateCharacterShow();
-        }
-        public void OnPreButtonClick()//当我们点击了上一个按钮
-        {
-            selectedIndex--;
-            if (selectedIndex == -1)
-            {
-                selectedIndex = length - 1;
-
-            }
-            UpdateCharacterShow();
-        }
-
-        //创建一个OK按钮
-
-        public void OnOkButtonClick()
-        {
-            //加载下一个场景
-            PlayerPrefs.SetInt("SelectedCharacterIndex", selectedIndex);//存储选择的角色
-            SceneManager.LoadScene(1);                                                            //加载下一个场景
+        UpdateCharacterShow();
     }
+
+    void UpdateCharacterShow()//更新所有角色的显示
+    {
+        characterGameObjects[selectedIndex].SetActive(true);
+        for (int i = 0; i < length; i++)
+        {
+            if (i != selectedIndex)
+            {
+                characterGameObjects[i].SetActive(false);//把未选择的角色隐藏起来
+            }
+        }
     }
+    public void OnNextButtonClick()//当我们点击了下一个按钮
+    {
+        selectedIndex++;
+        selectedIndex %= length;
+        UpdateCharacterShow();
+    }
+    public void OnPreButtonClick()//当我们点击了上一个按钮
+    {
+        selectedIndex--;
+        if (selectedIndex == -1)
+        {
+            selectedIndex = length - 1;
+
+        }
+        UpdateCharacterShow();
+    }
+
+    //创建一个OK按钮
+    public void OnOkButtonClick()
+    {
+        //加载下一个场景
+        PlayerPrefs.SetInt("SelectedCharacterIndex", selectedIndex);//存储选择的角色
+        SceneManager.LoadScene(1);                                                            //加载下一个场景
+    }
+}
 
